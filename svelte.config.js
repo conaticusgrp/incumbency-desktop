cb598468d4a1ad3ddbb1c1639a61dee84dd2d3a3
Patato <pathonscript@gmail.com>
2022-11-13 16:10:33 +0700

    setup tailwindcss

--- svelte.config.js
+++ svelte.config.js
@@ -3,5 +3,7 @@ import sveltePreprocess from "svelte-preprocess";
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess(),
+  preprocess: sveltePreprocess({
+    postcss: true,
+  }),
 };
