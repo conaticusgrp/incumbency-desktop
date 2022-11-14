c007dd132c5d134122b1093c8b72c04f0d6ad9b2
conaticus <enquiry.conaticus@gmail.com>
2022-11-14 11:30:20 +0000

    create new game save

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,36 @@
   windows_subsystem = "windows"
 )]
 
+const DATA_PATH: &str = "./data";
+const SAVES_PATH: &str = "./data/saves";
+
+fn check_data_directories() {
+  if !std::path::Path::new(DATA_PATH).exists() {
+    std::fs::create_dir(DATA_PATH).unwrap();
+    std::fs::create_dir(SAVES_PATH).unwrap();
+
+    return;
+  }
+
+  if !std::path::Path::new(SAVES_PATH).exists() {
+    std::fs::create_dir(SAVES_PATH).unwrap();
+  }
+}
+
+#[tauri::command]
+fn check_save_exists(name: String) -> bool {
+  check_data_directories();
+  std::path::Path::new(&format!("{}/{}", SAVES_PATH, name)).exists()
+}
+
+#[tauri::command]
+fn create_game(name: String) {
+  std::fs::create_dir(format!("{}/{}", SAVES_PATH, name)).unwrap();
+}
+
 fn main() {
   tauri::Builder::default()
+  .invoke_handler(tauri::generate_handler![create_game, check_save_exists])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
