6c4b4a2f6c4da025c2d3725959e82f4c6b5fb36e
VirusBLITZ <58221423+VirusBLITZ@users.noreply.github.com>
2022-11-16 16:39:55 +0100

    sync

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -30,9 +30,18 @@ fn create_game(name: String) {
   std::fs::create_dir(format!("{}/{}", SAVES_PATH, name)).unwrap();
 }
 
+#[tauri::command]
+fn list_saves() -> Vec<String> {
+  check_data_directories();
+  std::fs::read_dir(SAVES_PATH)
+    .unwrap()
+    .map(|entry| entry.unwrap().file_name().into_string().unwrap())
+    .collect()
+}
+
 fn main() {
   tauri::Builder::default()
-  .invoke_handler(tauri::generate_handler![create_game, check_save_exists])
+  .invoke_handler(tauri::generate_handler![create_game, check_save_exists, list_saves])
     .run(tauri::generate_context!())
     .expect("Error while running tauri application");
 }
--- src/components/views/LoadGameMenu.svelte
+++ src/components/views/LoadGameMenu.svelte
@@ -1,23 +1,66 @@
 <script lang="ts">
+  import { AppState, appState } from "../../stores/appState"
+  import { invoke } from "@tauri-apps/api/tauri"
 
-  import { AppState, appState } from "../../stores/appState";
+  let saves: string[] = []
 
-
+  const get_saves = async () => {
+    saves = (await invoke("list_saves")) as string[]
+  }
+  get_saves()
 </script>
 
 <main>
-
   <p>load game</p>
-  <button on:click={() => appState.set(AppState.MAIN_MENU)}>Back</button>
-
+  <div class="horizontal_scroll">
+    {#each saves as save}
+      <!-- change this to open game view -->
+      <div
+        on:mouseup={() => appState.set(AppState.MAIN_MENU)}
+        class="save_card"
+      >
+        {save}
+      </div>
+    {/each}
+  </div>
+  <button class="button_back" on:click={() => appState.set(AppState.MAIN_MENU)}
+    >Back</button
+  >
 </main>
 
 <style>
-
   main {
     position: relative;
     width: 100%;
     height: 100%;
   }
 
+  .horizontal_scroll {
+    display: flex;
+    flex-wrap: nowrap;
+    overflow-x: auto;
+  }
+
+  .save_card {
+    display: inline-block;
+    background-color: #1a1a1a;
+    margin: 0 1rem;
+    width: 18rem;
+    width: 300px;
+    height: 25rem;
+  }
+
+  button:hover:enabled {
+    border: none;
+  }
+  .save_card:hover {
+    filter: brightness(0.8);
+  }
+
+  .button_back {
+    position: absolute;
+    bottom: 0.5rem;
+    left: 50%;
+    transform: translate(-50%, 0);
+  }
 </style>
