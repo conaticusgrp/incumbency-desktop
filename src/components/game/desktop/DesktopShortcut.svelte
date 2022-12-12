0ec011fb53ddc0c193dcff213cb410327a4bce8f
VirusBLITZ <58221423+VirusBLITZ@users.noreply.github.com>
2022-12-12 15:16:49 +0100

    Create desktop and shortcuts (#12)
    * change bulde identifier to allow building for prod
    
    * sync
    
    * switch border radius to rem and create variable
    
    * use-select: none by default
    
    * create center class
    
    * save game overview
    
    * remove max-width from #app
    
    * create desktop
    
    * create desktop shortcut
    
    * open desktop when save is clicked
    
    * show desktop in singleplayer game component
    
    * align shortcuts vertically on desktop load

---
+++ src/components/game/desktop/DesktopShortcut.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  export let name = "?"
+  export let icon: string
+  export let badgeCount = 0
+</script>
+
+<desktopShortcut>
+  <div class="icon">
+    <!-- icon -->
+    <!-- <img src={icon} alt={name + " app icon"} /> -->
+    {#if badgeCount > 0}
+      <div id="badge">{badgeCount}</div>
+    {/if}
+  </div>
+  <div>{name}</div>
+</desktopShortcut>
+
+<style>
+  desktopShortcut {
+    width: 80px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    /* justify-content: center; */
+  }
+  .icon {
+    width: 70px;
+    height: 70px;
+    background: #3d3d3d;
+    background-size: cover;
+    position: relative;
+  }
+
+  .icon img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  #badge {
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    top: calc(100% - 10px);
+    left: calc(100% - 10px);
+    z-index: 1;
+    border-radius: 50%;
+    background: #c2403a;
+  }
+</style>
