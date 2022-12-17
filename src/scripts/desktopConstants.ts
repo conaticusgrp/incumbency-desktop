b52a9767e0402620c4e179beccadbea306330fcb
VirusBLITZ <58221423+VirusBLITZ@users.noreply.github.com>
2022-12-17 16:40:18 +0100

    change window header height to 25, remove duplicate exports

--- src/scripts/desktopConstants.ts
+++ src/scripts/desktopConstants.ts
@@ -1,9 +1,6 @@
 export const NOTIFICATIONS_WINDOW_WIDTH = 300;      // px
 export const NOTIFICATIONS_WINDOW_HEIGHT = 150;     // px
-export const WINDOW_HEADER_HEIGHT = 20;             // px
+export const WINDOW_HEADER_HEIGHT = 25;             // px
 export const RESIZE_BAR_SIZE = 5;                   // px
 export const MIN_WINDOW_WIDTH = 300;                // px
 export const MIN_WINDOW_HEIGHT = 200;               // px
-export const NOTIFICATIONS_WINDOW_WIDTH = 300;      // px
-export const NOTIFICATIONS_WINDOW_HEIGHT = 150;     // px
-export const WINDOW_HEADER_HEIGHT = 20;             // px
