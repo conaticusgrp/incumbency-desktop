3688190bc93014a7770bcf11bfb33c8bef625119
VirusBLITZ <58221423+VirusBLITZ@users.noreply.github.com>
2022-12-17 17:11:08 +0100

    reformat TestWindow

--- src/components/game/windows/TestWindow.svelte
+++ src/components/game/windows/TestWindow.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-  
-  import Window from "./Window.svelte";
-
-
+  import Window from "./Window.svelte"
 </script>
 
-<Window title="TestWindow" iconPath="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/How_to_use_icon.svg/1200px-How_to_use_icon.svg.png" pos={ { x: 100, y: 50 } } size={ { width: 800, height: 600 } }>
-
+<Window
+  title="TestWindow"
+  iconPath="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/How_to_use_icon.svg/1200px-How_to_use_icon.svg.png"
+  pos={{ x: 100, y: 50 }}
+  size={{ width: 800, height: 600 }}
+>
   <p>Test window</p>
   <!-- Write the HTML for the window here -->
-
 </Window>
 
 <style>
-
 </style>
