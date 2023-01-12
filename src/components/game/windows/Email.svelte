67b320b020b2e270c8d26fa7c07745c8a86c06c5
Ex_ce_pt <except.dice@gmail.com>
2023-01-12 19:20:10 +0100

    basic email layout

--- src/components/game/windows/Email.svelte
+++ src/components/game/windows/Email.svelte
@@ -5,7 +5,31 @@
 
   export let iconPath: string | undefined = undefined;
 
+  const EMAIL_LIST_WIDTH = 25; // %
+  const EMAIL_LIST_MIN_WIDTH = 200; // px
+  const EMAIL_MARGIN = 1; // em
+  const EMAIL_HEIGHT = 100; // px
+
   let dispatcher = createEventDispatcher();
+
+  // DEBUG
+  let emails = [
+    {
+      title: "Test 1",
+      content: "Lorem ipsum",
+      sender: "system"
+    },
+    {
+      title: "Test 2",
+      content: "Lorem ipsum",
+      sender: "system"
+    },
+    {
+      title: "Test 3",
+      content: "Lorem ipsum",
+      sender: "system"
+    },
+  ];
 
 </script>
 
@@ -17,10 +41,66 @@
   on:windowClose={() => dispatcher('windowClose')}
   on:windowMinimize={() => dispatcher('windowMinimizeStateChange')}
 >
-  <p>Email</p>
-  <!-- Write the HTML for the window here -->
+  <main class="content">
+
+    <div
+      class="email-list"
+      style="
+        width: {EMAIL_LIST_WIDTH}%;
+        min-width: {EMAIL_LIST_MIN_WIDTH}px;
+      "
+    >
+
+      {#each emails as email}
+
+      <div
+        class="email-list-entry"
+        style="
+          margin: {EMAIL_MARGIN}em;
+          height: {EMAIL_HEIGHT}px;
+        "
+      >
+          <h3>{email.title}</h3>
+          <p>{email.content}</p>
+      </div>
+
+      {/each}
+
+    </div>
+
+    <div
+      class="email-content"
+      style="width: calc(100% - {EMAIL_LIST_WIDTH}%);"
+    >
+
+    </div>
+    
+  </main>
 </Window>
 
 <style>
 
+  main {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 100%;
+  }
+
+  .email-list {
+    display: flex;
+    flex-direction: column;
+    overflow-y: scroll;
+  }
+
+  .email-list-entry {
+    text-align: left;
+    text-overflow: ellipsis;
+    color: black;
+    background-color: gainsboro;
+  }
+
+  .email-content{
+  }
+  
 </style>
