ebdc6ba5858684ba514976bfbb593de7b3ff7ba6
Ex_ce_pt <except.dice@gmail.com>
2023-01-24 21:27:23 +0100

    constants

--- src/components/game/desktop/Desktop.svelte
+++ src/components/game/desktop/Desktop.svelte
@@ -3,10 +3,7 @@
   import { listen } from "@tauri-apps/api/event";
   import type { DesktopAppShortcut } from "../../../scripts/desktopApp";
   import type { NotificationData } from "../../../scripts/notificationData";
-  import { DEFAULT_NOTIFICATION_DISPLAY_TIME, KEEP_NOTIFICATIONS_DISPLAYED, NOTIFICATIONS_WINDOW_HEIGHT, NOTIFICATIONS_WINDOW_WIDTH } from "../../../scripts/desktopConstants";
-  import DesktopShortcut from "./DesktopShortcut.svelte"
-  import ToolBarItem from "./ToolBarItem.svelte";
-  import Notification from "./Notification.svelte";
+  import { DATE_TIME_HEIGHT, TOOLBAR_HEIGHT } from "../../../scripts/desktopConstants";
 
   import Email from "../windows/Email.svelte";
   import BudgetPanel from "../windows/BudgetPanel.svelte";
@@ -15,13 +12,9 @@
   import { onMount } from 'svelte'
   import DebuggerApp from "../windows/DebuggerApp.svelte";
 
-  // TODO: desktop constants for min height
-
   let date: string = "undefined date";
   let windowContainer: HTMLElement;
   let appListWidthPixels = 300;
-  let toolbarHeightPercent: number = 7;
-  let dateTimeHeightPercent: number = 7;
   let wallpaperPath: string | null = null;
   let apps: DesktopAppShortcut[] = [
     { name: "DEBUG" },
@@ -36,12 +29,14 @@
     return windowContainer.children[index] as HTMLElement;
   }
 
+  /*
   $: if (notifications.length > 0 && !KEEP_NOTIFICATIONS_DISPLAYED) {
     setTimeout(() => {
       notifications.splice(0, 1);
       notifications = notifications; // trigger render
     }, (notifications[0].displayTime || DEFAULT_NOTIFICATION_DISPLAY_TIME) * 1_000);
   }
+  */
 
   const handleOpenApp = (e: CustomEvent): void => {
     const index = e.detail.index;
@@ -106,9 +101,15 @@
 
     <div class="app-list">
 
-      {#each apps as shortcut}
+      {#each apps as shortcut, i}
 
-      <div style="color: {false ? 'grey' : 'white'};">{shortcut.name}</div>
+      {@const appExists = windowContainer != null && windowContainer.children.length > i}
+      {@const appOpened = appExists &&
+                          ((windowContainer.children[i].style.display != 'none' &&
+                          windowContainer.children[i].dataset['minimized'] == 'false') ||
+                          (windowContainer.children[i].style.display == 'none' &&
+                          windowContainer.children[i].dataset['minimized'] == 'true'))}
+      <div style="color: {appOpened ? 'grey' : 'white'};">{shortcut.name}</div>
 
       {/each}
 
@@ -123,14 +124,14 @@
 
     <div
       class="date-time"
-      style="height: {dateTimeHeightPercent}%;"
+      style="height: {DATE_TIME_HEIGHT}%;"
     >
       {date}
     </div>
 
     <div
       class="windows"
-      style="height: {100 - dateTimeHeightPercent - toolbarHeightPercent}%"
+      style="height: {100 - DATE_TIME_HEIGHT - TOOLBAR_HEIGHT}%"
       bind:this={windowContainer}
     >
 
@@ -142,73 +143,20 @@
 
     <div
       class="toolbar"
-      style="height: {toolbarHeightPercent}%;"
+      style="height: {TOOLBAR_HEIGHT}%;"
     >
-
-    </div>
-
-  </div>
-
-</main>
-
-<!--
-<main style="background-image: {(wallpaperPath != null) ? `url(${wallpaperPath})` : "none"};">
-  
-  <div class="content" style="padding: {desktopPaddingRem}rem; height: calc({100 - toolbarHeightPercent}% - {desktopPaddingRem * 2}rem);">
-
-    {#each apps as shortcut, i}
-
-    {#if shortcut.name !== "DEBUG"}
-
-    <DesktopShortcut
-      name={shortcut.name}
-      badgeCount={shortcut.badgeCount}
-      gridRow={`${i} / ${i + 1}`}
-      index={i}
-      on:openApp={handleOpenApp}
-    />
-
-    {/if}
-
-    {/each}
-
-    <div class="windows" bind:this={windowContainer}>
-      <DebuggerApp on:windowClose={updateUI} on:windowMinimizeStateChange={updateUI} />
-      <Email on:windowClose={updateUI} on:windowMinimizeStateChange={updateUI} />
-      <BudgetPanel on:windowClose={updateUI} on:windowMinimizeStateChange={updateUI} />
-    </div>
-
-    <div class="notifications" style="width: {NOTIFICATIONS_WINDOW_WIDTH}px; height: {NOTIFICATIONS_WINDOW_HEIGHT}px;">
-      
-      {#if notifications.length > 0}
-
-      <Notification
-        iconPath={notifications[0].iconPath}
-        header={notifications[0].header}
-        content={notifications[0].content}
-      />
-
-      {/if}
-
-    </div>
-
-  </div>
-
-  <div class="toolbar" style="height: {toolbarHeightPercent}%;">
-
-    <div class="items">
-
-      <ToolBarItem name="logo" iconPath={undefined} />
-      <ToolBarItem name="Search" iconPath={undefined} />
 
       {#each apps as shortcut, i}
 
-      {#if windowContainer != null && windowContainer.children.length > i && apps[i].name !== "DEBUG" &&
-          ((windowContainer.children[i].style.display != 'none' && windowContainer.children[i].dataset['minimized'] == 'false') ||
-          (windowContainer.children[i].style.display == 'none' && windowContainer.children[i].dataset['minimized'] == 'true'))}
+      {#if windowContainer != null && windowContainer.children.length > i && (apps[i].name !== "DEBUG" || true)}
 
-      <!- !! to cast (boolean | undefined) to boolean ->
-      <!- empty on:keydown to supress a warning ->
+      {@const appShown = windowContainer.children[i].style.display != 'none'}
+      {@const appMinimized = windowContainer.children[i].dataset['minimized'] == 'true'}
+
+      {#if (appShown && !appMinimized) || (!appShown && appMinimized)}
+
+      <!-- !! to cast (boolean | undefined) to boolean -->
+      <!-- empty on:keydown to supress a warning -->
       <span
         data-minimized={!!apps[i].minimized}
         title={shortcut.name}
@@ -220,18 +168,15 @@
 
       {/if}
 
-      {/each}
-    </div>
+      {/if}
 
-    <div class="date-time">
-      {date}
+      {/each}
+    
     </div>
 
   </div>
 
 </main>
-
--->
 
 <style>
 
@@ -280,9 +225,10 @@
     display: flex;
     justify-content: center;
     align-items: center;
+    min-height: min-content;
     border-bottom: 1px solid green;
   }
-
+  
   .windows {
     position: relative;
     z-index: 100;
@@ -294,26 +240,20 @@
     /* width: 100%; */
     display: flex;
     flex-direction: row;
+    min-height: min-content;
     border-top: 1px solid green;
+  }
+  
+  .toolbar > span {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 2em 0 2em;
+    border: 1px solid green;
   }
 
   /* to be updated */
-  
-  .toolbar > .items {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-  }
-  
-  .toolbar > .items > span {
-    position: relative;
-    width: 50px;
-    height: 50px;
-    margin: 0.5rem;
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
-  }
 
   .toolbar > .items > span[data-minimized="true"]:after {
     content: '';
@@ -323,15 +263,6 @@
     width: 100%;
     height: 100%;
     background-color: rgba(0, 0, 0, 0.5);
-  }
-
-  .notifications {
-    position: absolute;
-    top: 0;
-    right: 0;
-    /* background-color: #4A4A4A; */
-    z-index: 200;
-    pointer-events: none;
   }
 
 </style>
--- src/scripts/desktopConstants.ts
+++ src/scripts/desktopConstants.ts
@@ -2,11 +2,9 @@ export const RESIZE_BAR_SIZE = 5;                   // px
 export const MIN_WINDOW_WIDTH = 300;                // px
 export const MIN_WINDOW_HEIGHT = 200;               // px
 export const WINDOW_HEADER_HEIGHT = 25;             // px
-export const NOTIFICATIONS_WINDOW_WIDTH = 400;      // px
-export const NOTIFICATIONS_WINDOW_HEIGHT = 200;     // px
-export const NOTIFICATION_HEADER_HEIGHT = 30;       // px
-export const NOTIFICATION_PADDING = 16;             // px
-export const DEFAULT_NOTIFICATION_DISPLAY_TIME = 5; // s
 
-// DEBUG
+export const DATE_TIME_HEIGHT = 7;                  // %
+export const TOOLBAR_HEIGHT = 7;                    // %
+
+// DEBUG, to be used
 export const KEEP_NOTIFICATIONS_DISPLAYED = false;
