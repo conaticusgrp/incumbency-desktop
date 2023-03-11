dce7051c27fcf44f3c97c705cc824633d10feaeb
conaticus <enquiry.conaticus@gmail.com>
2023-03-11 16:14:07 +0000

    mistakes were made

--- src/components/game/windows/Window.svelte
+++ src/components/game/windows/Window.svelte
@@ -294,7 +294,7 @@
                 data: {
                     title: "The Start of Your Incumbency",
                     content: `
-Hello, ${USERNAME}. You are now in leading position for the countries economy. Gary set the bar high during his incumbency, however, his sex scandal led him to step down from office. My name is Ned, I will be here to guide you during your time in office.
+Hello, ${USERNAME}. You are now in leading position for the country's economy. Gary set the bar high during his incumbency, however, his sex scandal led him to step down from office. My name is Ned, I will be here to guide you during your time in office.
 
 What you are currently looking at is the desktop environment that you will use to manage the economy. From here you will control everything - taxes, laws & regulations, healthcare and more. Take a look at your left panel and you will see all the available desktop apps that are at your disposal. You can open them and take a look around. Just.. try not to break everything, okay?
 
