163a106ad75c370f05c76bf207913e55ededc05c
Dylan Hackworth <followmelikethunder@pm.me>
2023-04-01 14:10:22 -0500

    ref: expected graph instead

--- src/components/ui/graphs/PredictedGraph.svelte
+++ src/components/ui/graphs/PredictedGraph.svelte
@@ -1,22 +1,27 @@
 <script lang="ts">
   import type { ChartDataset } from "chart.js";
-  import { listen } from "svelte/internal";
   import LineGraph from "../LineGraph.svelte";
 
+  type Data = {
+    label: string;
+    data: number[];
+  }
+  export let expected: Data;
+  export let actual: Data;
 
   // TODO(dylhack): Convert to days/weeks/months/years 
   //                based on what the backend gives us
   const labels = ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7'];
   // TODO(dylhack): Get actual data from backend
   const datasets: ChartDataset<'line', number[]>[] = [{
-    label: 'Actual Government Balance',
-    data: [],
+    label: actual.label,
+    data: actual.data,
     borderColor: '#5B8211',
     pointBorderWidth: 0,
   },
   {
-    label: 'Expected Government Balance',
-    data: [],
+    label: expected.label,
+    data: expected.data,
     borderColor: '#A72E2E',
     borderDash: [5, 5],
     pointBorderWidth: 0,
